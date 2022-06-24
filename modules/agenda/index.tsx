9f5b555b6a18bd9802eb5abbe4c79b1f5ac3cd12
unknown <muhammadumer.hasan97@gmail.com>
2022-06-25 02:02:48 +0500

    import data

--- modules/agenda/index.tsx
+++ modules/agenda/index.tsx
@@ -3,13 +3,15 @@ import { Paper,Button } from '@material-ui/core'
 import { DataGrid } from '@mui/x-data-grid';
 import Header from '../../shared/Header/header'
 import Create from './create'
-import { truncate } from 'fs';
-import { setServers } from 'dns';
+import Papa from "papaparse";
+
 import { CSVLink } from "react-csv";
+
+
 function Index() {
 
     const [open,setOpen] = useState(false);
-
+    const [selected,setSelected] = useState({});
     const row = [
         {
             id:0,
@@ -77,33 +79,34 @@ function Index() {
                     >
                         Delete
                     </Button>
+                  
                 </>
                )
             }
         },
     ]
 
-    const [rows,setRows] = useState([
-        {
-            id:0,
-            title:"Standhup meeting",
-            description:"To discuss daily tasks",
-            status:"pending",
-            time: new Date().toUTCString()
-        },
-        {
-            id:1,
-            title:"Standhup meeting",
-            description:"To discuss daily tasks",
-            status:"pending",
-            time: new Date().toUTCString()
-        },
-        {   id:2,
-            title:"Standhup meeting",
-            description:"To discuss daily tasks",
-            status:"pending",
-            time: new Date().toUTCString()
-        }
+    const [rows,setRows] = useState<any>([
+        // {
+        //     id:0,
+        //     title:"Standhup meeting",
+        //     description:"To discuss daily tasks",
+        //     status:"pending",
+        //     time: new Date().toUTCString()
+        // },
+        // {
+        //     id:1,
+        //     title:"Standhup meeting",
+        //     description:"To discuss daily tasks",
+        //     status:"pending",
+        //     time: new Date().toUTCString()
+        // },
+        // {   id:2,
+        //     title:"Standhup meeting",
+        //     description:"To discuss daily tasks",
+        //     status:"pending",
+        //     time: new Date().toUTCString()
+        // }
 
     ])
     
@@ -133,10 +136,51 @@ function Index() {
 
 
     }
+
+
   return (
       <>
       <Header name={'Agneda List'} callOpen={callOpen}/>
       <Paper>
+      <div style={{padding:'10px'}}>
+       
+      <input
+        type="file"
+        accept=".csv,.xlsx,.xls"
+        onChange={(e) => {
+          const files = e.target.files;
+          console.log(files);
+          if (files) {
+            console.log(files[0]);
+            Papa.parse(files[0], {
+              complete: function(results) {
+                
+
+                let data:any[]=results.data;
+                let arr:any = [];
+                let keys = data[0];
+                for(var i=1;i<data.length;i++)
+                {
+                    let entry:any = {};
+                    data[i].map((e:any,index:any)=>{
+
+                        entry[keys[index]]= e
+                    })
+                    arr.push(entry)
+                    // for(var j=0; j<data[i].length;j++)
+                    // {
+                    //     entry[data[0]]: data[i];
+                    // }
+                }
+                setRows(arr);
+                //console.log("Finished:", arr);
+              }}
+            )
+          }
+        }}
+      />
+    
+    </div>
       <div style={{ height: 400, width: '100%',padding:'10px 0px' }}>
      <Button style={{ margin:'10px 10px'}}
         variant="outlined"
@@ -153,7 +197,7 @@ function Index() {
       </Paper>
 
         {
-            open &&  <Create open={open} onClose={setOpen} AddAgenda={AddAgenda}/>
+            open &&  <Create open={open} onClose={setOpen} AddAgenda={AddAgenda} />
         }
        
       
